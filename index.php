2ed64db763b34d4a5918ad3c3e1a7083edadf76a
fathana <fathan.aa.01@gmail.com>
2021-01-22 12:53:44 +0700

    push

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
+    <title>minimarket</title>
+</head>
+<body>
+    
+        <header class="navbar flex">
+            <div id="judul">
+                <h3>minimarket</h3>
+            </div>
+            <div id="menu">
+                <ul>
+                    <li><a href="#">beranda</a></li>
+                    <li><a href="#">kategori</a></li>
+                    <li><a href="#">checkout</a></li>
+                </ul>
+            </div>
+        </header>
+
+        <section class="barang container">
+          <div class="row">
+            <div class="col">
+              <p>Lorem ipsum dolor sit.</p>
+            </div>
+            <div class="col">
+              <p>Lorem ipsum dolor sit.</p>
+            </div>
+            <div class="col">
+              <p>Lorem ipsum dolor sit.</p>
+            </div>
+            <div class="col">
+              <p>Lorem ipsum dolor sit.</p>
+            </div>
+          </div>
+        </section>
+
+</body>
+</html>
