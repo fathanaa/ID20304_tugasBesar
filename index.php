26243629ff16fbe11a1a8c8b1dba7e81fce45faa
fathana <fathan.aa.01@gmail.com>
2021-01-24 14:53:08 +0700

    asd

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ require 'config.php';
 <body>
   <header class="navbar flex">
     <div id="judul">
-        <h3><a href="#">minimarket</a></h3>
+        <h3><a style="color:black;" href="#">minimarket</a></h3>
     </div>
     <div id="menu">
         <ul>
@@ -32,15 +32,20 @@ require 'config.php';
         </ul>
     </div>
   </header>
+
+  <div class="totalBelanja container">
+    <h4>Total : </h4>
+  </div>
+
 <table class="table table-borderd container">
 <tr>
   <th>No</th>
   <th>Nama Barang</th>
   <th>Gambar</th>
   <th>Berat/pcs (g)</th>
-  <th>kategori</th>
   <th>Jumlah</th>
   <th>Harga/pcs (Rp)</th>
+  
 </tr>
 <?php
   $no=0;
@@ -54,18 +59,29 @@ foreach($cursor as $barang) {
   $no++;
   echo "<tr>";
     echo "<td>".$no."</td>";
+    echo "<td>".$barang->kode_barang."</td>";
     echo "<td>".$barang->nama_barang."</td>";
     echo "<td><img style='width:100px;' class='gambar' src='image/kb_0".$no.".jpeg'></td>";
     echo "<td>".$barang->berat."</td>";
-    echo "<td>".$barang->kategori_barang[1]."</td>";
     echo "<td>".$barang->jumlah."</td>";
     echo "<td>".$barang->harga."</td>";
-    
+    echo "<td><button class='plus' ;'>+</button><button class='min'  >-</button></td>";
   echo "</tr>";
 };
 
 ?>
 
 </table>
+
+
+<style>
+  .totalBelanja h4{
+    padding: 20px;
+}
+  .plus, .min{
+    padding:10px;
+    font-size: 20px;
+  }
+</style>
 </body>
 </html>
